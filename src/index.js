4fd49b5b69c4d4e6921c44115d2ce86248bcf324
xingychb <xingychb@yonyou.com>
2020-03-19 17:13:26 +0800

    修改

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 function HelloMessage(props) {
+    const windowLocationOrigin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port : '');//ie8-ie10不兼容的原因
+
+    const login_server = getHost(),
+        yhtssoisloginUrl = windowLocationOrigin + '/login_light',
+        _destUrl = `/yhtssoislogin`, //只有这个不是登陆成功后跳转的链接
+        realservice = windowLocationOrigin;
     return (
-        <div className="aaa">
-            <h1>Hello World!</h1>
+        <div className="wrap">
+            <div className="left">
+                <img src="./static/logo.png" className="logo" title="yonbip" />
+                <iframe
+                    src={`${login_server}/cas/login?sysid=yonsuite&mode=light&service=${encodeURIComponent(yhtssoisloginUrl + '?yhtdesturl=' + _destUrl + '&yhtrealservice=' + realservice)}&locale=${getCurrentLangCode()}`}
+                    frameborder="0"
+                    width="450px"
+                    height="373px"
+                    className="iframe"
+                />
+                <p className="footer">用友网络科技股份有限公司 © Copyright 2019 京ICP备05007539号-24 京ICP证100714号</p>
+            </div>
+            <div className="right">
+                <div className="header">
+                    <span>商业创新 如此便捷</span>
+                    <p>数智时代的商业创新平台</p>
+                </div>
+                <span className="applyUse">申请试用</span>
+                <img src="./static/pic.png" alt="" />
+            </div>
         </div>
     );
 }
